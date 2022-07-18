3309b32871394cd726d79a6005dc0aa6c5d0355b
JaeJun <jejun97@gmail.com>
2022-07-18 11:46:56 +0900

    add cards compoenents

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CardList from './CardList';
 import './index.css';
 import 'tachyons';
+import { robots } from './robots';
 
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(null, document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
 reportWebVitals();
